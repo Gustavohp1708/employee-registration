e1c7d39d2404fc48ffb1c2686ec6508c19278dce
User <gustavo.dev17@outlook.com>
2026-05-14 18:19:43 -0300

    create project

---
+++ src/test/java/io/github/gustavohp1708/employeeregistration/EmployeeRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.gustavohp1708.employeeregistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeRegistrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
