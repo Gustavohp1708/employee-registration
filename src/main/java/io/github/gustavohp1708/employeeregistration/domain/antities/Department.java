6ee461641159db1f72d9d73863e9164d29a893e2
User <gustavo.dev17@outlook.com>
2026-05-15 17:32:03 -0300

    create Department enum

---
+++ src/main/java/io/github/gustavohp1708/employeeregistration/domain/antities/Department.java
@@ -0,0 +1,10 @@
+package io.github.gustavohp1708.employeeregistration.domain.antities;
+
+public enum Department {
+
+    ESTAGIÁRIO,
+    TRAINEE,
+    ANALISTA_JUNIOR,
+    ANALISTA_PLENO,
+    ANALISTA_SENIOR;
+}
