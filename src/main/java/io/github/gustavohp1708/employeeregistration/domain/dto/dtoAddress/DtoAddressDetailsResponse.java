a1427c1e1250ed81b325bbb8c94e44fa5671b876
User <gustavo.dev17@outlook.com>
2026-05-15 17:32:17 -0300

    create DtoAddressDetailsResponse

---
+++ src/main/java/io/github/gustavohp1708/employeeregistration/domain/dto/dtoAddress/DtoAddressDetailsResponse.java
@@ -0,0 +1,28 @@
+package io.github.gustavohp1708.employeeregistration.domain.dto.dtoAddress;
+
+import io.github.gustavohp1708.employeeregistration.domain.antities.Address;
+
+public record DtoAddressDetailsResponse(
+
+        Long id,
+        String street,
+        String neighborhood,
+        String postalCode,
+        String number,
+        String complement,
+        String city,
+        String state
+) {
+    public DtoAddressDetailsResponse(Address address) {
+        this(
+                address.getId(),
+                address.getStreet(),
+                address.getNeighborhood(),
+                address.getPostalCode(),
+                address.getNumber(),
+                address.getComplement(),
+                address.getCity(),
+                address.getState()
+        );
+    }
+}
